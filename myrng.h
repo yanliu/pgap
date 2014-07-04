119fbaf34cbeaad2c9a74283280aac574f39c0e5
Yan Y. Liu <yanliu@illinois.edu>
2014-07-04 18:38:17 -0500

    fixed a bug in myrng.h. use (kkk) insted of kkk

--- myrng.h
+++ myrng.h
@@ -3,14 +3,14 @@
 
 // macro: random number generator
 #ifdef GSL_SPRNG
-#define MYRANDI(kkk) ((int)(gsl_rng_uniform_int(gsl_sprng_r, (unsigned long int)kkk)))
+#define MYRANDI(kkk) ((int)(gsl_rng_uniform_int(gsl_sprng_r, (unsigned long int)(kkk))))
 #define MYRANDF() (gsl_rng_uniform(gsl_sprng_r))
 #else
 #ifdef SPRNG
-#define MYRANDI(kkk) ((int)(mysprng() * kkk))
+#define MYRANDI(kkk) ((int)(mysprng() * (kkk)))
 #define MYRANDF() (mysprng())
 #else
-#define MYRANDI(kkk) ((int)(drand48() * kkk))
+#define MYRANDI(kkk) ((int)(drand48() * (kkk)))
 #define MYRANDF() (drand48())
 #endif
 #endif
